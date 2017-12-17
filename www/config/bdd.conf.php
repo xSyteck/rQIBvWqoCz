d1a3e20fa695376d61b037dc295a4bb3d201c251
Thibault <thibaultloison1@gmail.com>
2017-12-17 23:14:25 +0100

    1.0
    Final version

--- www/config/bdd.conf.php
+++ www/config/bdd.conf.php
@@ -1,7 +1,7 @@
 <?php
 try{
 	/* @var $bdd PDO */
-	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root');
+	$bdd = new PDO('mysql:host=localhost;dbname=id3521846_bdd;charset=utf8', 'id3521846_root', 'Azerty123456');
 	$bdd->exec("set names utf8");
 	$bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
